52c15e360033efe8f9c5692e5aa7571675eb72a5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-08-03 20:00:13 +1200

    Add basic test.

--- test/async/rails.rb
+++ test/async/rails.rb
@@ -3,3 +3,8 @@
 # Released under the MIT License.
 # Copyright, 2025, by Samuel Williams.
 
+describe Async::Rails do
+	it "has a version number" do
+		expect(Async::Rails::VERSION).to be =~ /^\d+\.\d+\.\d+$/
+	end
+end
